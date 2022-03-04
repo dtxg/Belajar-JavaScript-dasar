75509ab15451333d9863eb4840e416d5766568d2
dtxg <100848749+dtxg@users.noreply.github.com>
2022-03-04 14:59:32 +0700

    belajar pengulangan menggunakan while

---
+++ whileLoop.js
@@ -0,0 +1,15 @@
+// while loop
+
+var i = 1;
+
+while(i <= 5){
+    console.log('Loop ke '+i);
+    // increment
+    i++;
+}
+
+// for loop
+
+for(var x = 1; x <= 5; x++){
+    console.log('looping ke '+ x +'x');
+}
