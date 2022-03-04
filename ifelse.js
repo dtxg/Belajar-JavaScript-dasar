e22fe44d17e9aca6c41679e9e281174b2353ef8b
dtxg <100848749+dtxg@users.noreply.github.com>
2022-03-04 15:28:31 +0700

    mengecek type data

--- ifelse.js
+++ ifelse.js
@@ -1,8 +1,10 @@
 
 var i = 1;
 
-if(i % 2 === 0){
+if(i % 2 == 0){
     console.log(i+' dalah bilangan genap');
-} else {
+} else if(i % 2 == 1){
     console.log(i+' dalah bilangan ganjil');
+}else {
+    console.log( typeof(i)+" => "+ null );
 }
