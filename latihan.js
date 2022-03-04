46f9ccf03b49edfb17c80b51077734e0ec0bb750
dtxg <100848749+dtxg@users.noreply.github.com>
2022-03-04 16:16:54 +0700

    latihan logika

--- latihan.js
+++ latihan.js
@@ -3,9 +3,9 @@ var angkotBeroperasi = 6;
 var noAngkot = 1;
 
 for(var noAngkot = 1; noAngkot <= jmlAngkot; noAngkot++){
-    if(noAngkot <= angkotBeroperasi){
+    if(noAngkot <= angkotBeroperasi && noAngkot !== 5){
         console.log('Angkot No. '+noAngkot+' sedang beroperasi dengan baik');
-    } else if(noAngkot == 8){
+    } else if(noAngkot === 8 || noAngkot === 10 || noAngkot === 5){
         console.log('Angkot No. '+noAngkot+' sedang lembur');
     }else {
         console.log('Angkot No. '+noAngkot+' sedang tidak beroperasi');
